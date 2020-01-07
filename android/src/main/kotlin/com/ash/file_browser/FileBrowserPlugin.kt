3e2664747bbd0d0861e3f36d88baabde251ab34d
chen zhen <zhen.chen@orgneering.com>
2020-01-07 14:44:06 +0800

    init

---
+++ android/src/main/kotlin/com/ash/file_browser/FileBrowserPlugin.kt
@@ -0,0 +1,27 @@
+package com.ash.file_browser
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FileBrowserPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "file_browser")
+      channel.setMethodCallHandler(FileBrowserPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else if(call.method == "pickDocument"){
+      result.success("over there")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
